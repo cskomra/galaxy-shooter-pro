d76556f56312fb07efe7319c0915070739c75520
Connie Skomra <connie@cskomra.com>
2021-06-22 15:43:37 -0400

    add Laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 5.0f;
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move player to the right
-        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        CalculateMovement();  
+
+        // given I hit the space key
+        // then Player fires a Laser prefab    
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }  
+    }
+
+    void CalculateMovement(){
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float upperBounds = 0f;
+        float lowerBounds = -3.8f;
+        float rightBounds = 11.3f;
+        float leftBounds = -11.3f;
+
+        // move player
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        //restrict bounds
+        if(transform.position.y >= upperBounds){
+            transform.position = new Vector3(transform.position.x, upperBounds, transform.position.z);
+        }else if(transform.position.y <= lowerBounds){
+            transform.position = new Vector3(transform.position.x, lowerBounds, transform.position.z);
+        }
+
+        if(transform.position.x >= rightBounds){
+            transform.position = new Vector3(-rightBounds, transform.position.y, transform.position.z);
+        }else if(transform.position.x <= leftBounds){
+            transform.position = new Vector3(-leftBounds, transform.position.y, transform.position.z);
+        }
     }
 }

f3bda5962a009824782b12972b5a13311d7f9136
Connie Skomra <connie@cskomra.com>
2021-06-24 14:47:02 -0400

    feat: spawn manager, damage ctrl, assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private bool _keepSpawning = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGameObjects(5.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // spawn game objects every 5 seconds
+    private IEnumerator SpawnGameObjects(float waitTime){
+        while(_keepSpawning){
+            Vector3 spawnPos = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject enemy = Instantiate(_enemyPrefab, spawnPos, Quaternion.identity);
+            enemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void OnPlayerDeath(){
+        _keepSpawning = false;
+    }
+}
